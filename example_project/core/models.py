da7867140e0c3a4c1f403334f8679d954b588f92
Nick Moreton <nickmoreton@me.com>
2025-07-08 21:17:48 +0100

    Add example page models

---
+++ example_project/core/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+
+class ExamplePageModelBasic(Page):
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
+
+
+class ExamplePageModelStandard(Page):
+    intro = models.TextField(blank=True)
+    body = RichTextField(blank=True)
+
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro'),
+        FieldPanel('body'),
+    ]
