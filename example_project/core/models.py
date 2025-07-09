29919980a655bb8915a54dddfbfe5235e463687e
Nick Moreton <nickmoreton@me.com>
2025-07-09 09:53:34 +0100

    quick save

--- example_project/core/models.py
+++ example_project/core/models.py
@@ -4,20 +4,42 @@ from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 
 
-class ExamplePageModelBasic(Page):
-    body = RichTextField(blank=True)
+class BasePage(Page):
+    """
+    Base class for all pages in the example project.
+    This can be extended by other page models to include common fields or methods.
+    """
+    class Meta:
+        abstract = True
+
+    banner_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text="Optional banner image for the page."
+    )
 
     content_panels = Page.content_panels + [
+        FieldPanel('banner_image'),
+    ]
+
+
+class ExamplePageModelBasic(BasePage):
+    body = RichTextField(blank=True)
+
+    content_panels = BasePage.content_panels + [
         FieldPanel('body'),
     ]
 
 
-class ExamplePageModelStandard(Page):
+class ExamplePageModelStandard(BasePage):
     intro = models.TextField(blank=True)
     body = RichTextField(blank=True)
 
 
-    content_panels = Page.content_panels + [
+    content_panels = BasePage.content_panels + [
         FieldPanel('intro'),
         FieldPanel('body'),
     ]
---
+++ example_project/static/js/example_project.js
@@ -0,0 +1,2 @@
+// Just so we know this file has loaded
+console.log('example_project.js loaded');
