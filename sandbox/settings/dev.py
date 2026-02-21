0ad040721574174544d58a6918b9f73568bce3c6
Nick Moreton <nickmoreton@me.com>
2026-02-21 21:44:11 +0000

    Initial

---
+++ sandbox/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-^*^xj-&@p(ex2p#jgw%t_9q$#8stnoa*nt8w=1j1c+os&j7*8v"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
